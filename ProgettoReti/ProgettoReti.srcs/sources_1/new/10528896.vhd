fc561d5821374da4e12eb1338dd0df5853c62772
Desno365 <desno365@gmail.com>
2019-02-22 18:14:26 +0100

    Salta centroidi da non considerare per maschera di input

--- ProgettoReti/ProgettoReti.srcs/sources_1/new/10528896.vhd
+++ ProgettoReti/ProgettoReti.srcs/sources_1/new/10528896.vhd
@@ -38,6 +38,55 @@ architecture projectArch of project_reti_logiche is
     
     signal distance_tot : unsigned(8 downto 0) := "111111111"; -- 9 bit perchè massima distanza è 255 + 255 = 510
     
+    procedure get_next_state_and_address(
+    in_index : in natural range 0 to 7;
+    in_mask : in std_logic_vector(7 downto 0);
+    signal out_state : out state_type;
+    signal out_address : out std_logic_vector(15 downto 0)) is 
+    begin
+        if(in_mask(0) = '1' and in_index <= 0) then
+            out_state <= S_C1X;
+            out_address <= std_logic_vector(START_ADDRESS + 1); -- 1 X centroide 1
+        else
+            if(in_mask(1) = '1' and in_index <= 1) then
+                out_state <= S_C2X;
+                out_address <= std_logic_vector(START_ADDRESS + 3); -- 3 X centroide 2
+            else
+                if(in_mask(2) = '1' and in_index <= 2) then
+                    out_state <= S_C3X;
+                    out_address <= std_logic_vector(START_ADDRESS + 5); -- 5 X centroide 3
+                else
+                    if(in_mask(3) = '1' and in_index <= 3) then
+                        out_state <= S_C4X;
+                        out_address <= std_logic_vector(START_ADDRESS + 7); -- 7 X centroide 4
+                    else
+                        if(in_mask(4) = '1' and in_index <= 4) then
+                            out_state <= S_C5X;
+                            out_address <= std_logic_vector(START_ADDRESS + 9); -- 9 X centroide 5
+                        else
+                            if(in_mask(5) = '1' and in_index <= 5) then
+                                out_state <= S_C6X;
+                                out_address <= std_logic_vector(START_ADDRESS + 11); -- 11 X centroide 6
+                            else
+                                if(in_mask(6) = '1' and in_index <= 6) then
+                                    out_state <= S_C7X;
+                                    out_address <= std_logic_vector(START_ADDRESS + 13); -- 13 X centroide 7
+                                else
+                                    if(in_mask(7) = '1' and in_index <= 7) then
+                                        out_state <= S_C8X;
+                                        out_address <= std_logic_vector(START_ADDRESS + 15); -- 15 X centroide 8
+                                    else
+                                        out_state <= S_C8Y;
+                                        out_address <= "----------------";
+                                    end if;
+                                end if;
+                            end if;
+                        end if;
+                    end if;
+                end if;
+            end if;
+        end if;
+    end get_next_state_and_address;
     
     begin
     
@@ -66,7 +115,7 @@ architecture projectArch of project_reti_logiche is
     end process;
     
     -- Gestione dello stato del componente
-    state_manager: process(current_state, i_start)
+    state_manager: process(current_state, i_start, input_mask_signal)
     begin
         case current_state is
             when S_RST =>
@@ -75,58 +124,101 @@ architecture projectArch of project_reti_logiche is
                 else
                     next_state <= S_RST;
                 end if;
+                o_address <= "----------------";
+
             when S_START =>
                 next_state <= S_INPUT_MASK;
+                o_address <= std_logic_vector(START_ADDRESS); -- 0 input mask
+
             when S_INPUT_MASK =>
-                next_state <= S_COORD_X;
+                if(input_mask_signal = "00000000") then
+                    next_state <= S_C8Y; -- va direttamente alla scrittura del dato di uscita
+                    o_address <= "----------------";
+                else
+                    next_state <= S_COORD_X;
+                    o_address <= std_logic_vector(START_ADDRESS + 17); -- 17 X del punto da valutareo_address <= std_logic_vector(START_ADDRESS + 17); -- 17 X del punto da valutare
+                end if;
+
             when S_COORD_X =>
                 next_state <= S_COORD_Y;
+                o_address <= std_logic_vector(START_ADDRESS + 18); -- 18 Y del punto da valutare
+
             when S_COORD_Y =>
-                next_state <= S_C1X;
+                get_next_state_and_address(0, input_mask_signal, next_state, o_address);
+
             when S_C1X =>
                 next_state <= S_C1Y;
+                o_address <= std_logic_vector(START_ADDRESS + 2); -- 2 Y centroide 1
+
             when S_C1Y =>
-                next_state <= S_C2X;
+                get_next_state_and_address(1, input_mask_signal, next_state, o_address);
+
             when S_C2X =>
                 next_state <= S_C2Y;
+                o_address <= std_logic_vector(START_ADDRESS + 4); -- 4 Y centroide 2
+
             when S_C2Y =>
-                next_state <= S_C3X;
+                get_next_state_and_address(2, input_mask_signal, next_state, o_address);
+
             when S_C3X =>
                 next_state <= S_C3Y;
+                o_address <= std_logic_vector(START_ADDRESS + 6); -- 6 Y centroide 3
+
             when S_C3Y =>
-                next_state <= S_C4X;
+                get_next_state_and_address(3, input_mask_signal, next_state, o_address);
+
             when S_C4X =>
                 next_state <= S_C4Y;
+                o_address <= std_logic_vector(START_ADDRESS + 8); -- 8 Y centroide 4
+
             when S_C4Y =>
-                next_state <= S_C5X;
+                get_next_state_and_address(4, input_mask_signal, next_state, o_address);
+
             when S_C5X =>          
                 next_state <= S_C5Y;
+                o_address <= std_logic_vector(START_ADDRESS + 10); -- 10 Y centroide 5
+
             when S_C5Y =>
-                next_state <= S_C6X;
+                get_next_state_and_address(5, input_mask_signal, next_state, o_address);
+
             when S_C6X =>
                 next_state <= S_C6Y;
+                o_address <= std_logic_vector(START_ADDRESS + 12); -- 12 Y centroide 6
+
             when S_C6Y =>
-                next_state <= S_C7X;
+                get_next_state_and_address(6, input_mask_signal, next_state, o_address);
+
             when S_C7X =>
                 next_state <= S_C7Y;
+                o_address <= std_logic_vector(START_ADDRESS + 14); -- 14 Y centroide 7
+
             when S_C7Y =>
-                next_state <= S_C8X;
+                get_next_state_and_address(7, input_mask_signal, next_state, o_address);
+
             when S_C8X =>
                 next_state <= S_C8Y;
+                o_address <= std_logic_vector(START_ADDRESS + 16); -- 16 Y centroide 8
+
             when S_C8Y =>
                 next_state <= S_DONE;
+                o_address <= std_logic_vector(START_ADDRESS + 19); -- 19 out mask
+
             when S_DONE =>
                 if(i_start = '1') then
                     next_state <= S_DONE;
                 else
                     next_state <= S_END;
                 end if;
+                o_address <= "----------------";
+
             when S_END =>
                 if(i_start = '1') then
                     next_state <= S_START;
                 else
                     next_state <= S_END;
                 end if;
+                o_address <= "----------------";
+
         end case;
     end process;
     
@@ -222,54 +314,6 @@ architecture projectArch of project_reti_logiche is
             end if;
         end if;
     end process;
-
-    mem_out_manager: process(current_state)
-    begin
-        case current_state is
-            when S_START =>
-                o_address <= std_logic_vector(START_ADDRESS); -- 0 input mask
-            when S_INPUT_MASK =>
-                o_address <= std_logic_vector(START_ADDRESS + 17); -- 17 X del punto da valutare
-            when S_COORD_X =>
-                o_address <= std_logic_vector(START_ADDRESS + 18); -- 18 Y del punto da valutare
-            when S_COORD_Y =>
-                o_address <= std_logic_vector(START_ADDRESS + 1); -- 1 X centroide 1
-            when S_C1X =>
-                o_address <= std_logic_vector(START_ADDRESS + 2); -- 2 Y centroide 1
-            when S_C1Y =>
-                o_address <= std_logic_vector(START_ADDRESS + 3); -- 3 X centroide 2
-            when S_C2X =>
-                o_address <= std_logic_vector(START_ADDRESS + 4); -- 4 Y centroide 2
-            when S_C2Y =>
-                o_address <= std_logic_vector(START_ADDRESS + 5); -- 5 X centroide 3
-            when S_C3X =>
-                o_address <= std_logic_vector(START_ADDRESS + 6); -- 6 Y centroide 3
-            when S_C3Y =>
-                o_address <= std_logic_vector(START_ADDRESS + 7); -- 7 X centroide 4
-            when S_C4X =>
-                o_address <= std_logic_vector(START_ADDRESS + 8); -- 8 Y centroide 4
-            when S_C4Y =>
-                o_address <= std_logic_vector(START_ADDRESS + 9); -- 9 X centroide 5
-            when S_C5X =>
-                o_address <= std_logic_vector(START_ADDRESS + 10); -- 10 Y centroide 5
-            when S_C5Y =>
-                o_address <= std_logic_vector(START_ADDRESS + 11); -- 11 X centroide 6
-            when S_C6X =>
-                o_address <= std_logic_vector(START_ADDRESS + 12); -- 12 Y centroide 6
-            when S_C6Y =>
-                o_address <= std_logic_vector(START_ADDRESS + 13); -- 13 X centroide 7
-            when S_C7X =>
-                o_address <= std_logic_vector(START_ADDRESS + 14); -- 14 Y centroide 7
-            when S_C7Y =>
-                o_address <= std_logic_vector(START_ADDRESS + 15); -- 15 X centroide 8
-            when S_C8X =>
-                o_address <= std_logic_vector(START_ADDRESS + 16); -- 16 Y centroide 8
-            when S_C8Y =>
-                o_address <= std_logic_vector(START_ADDRESS + 19); -- 19 out mask
-            when others =>
-                o_address <= "----------------"; -- Don't care siccome in questi stati si ha o_en = '0'
-        end case;
-    end process;
     
     -- Gestisce gli output di enable per la RAM
     mem_rw_manager: process(current_state)
@@ -280,7 +324,7 @@ architecture projectArch of project_reti_logiche is
             when S_START =>
                 o_en <= '1'; -- Input mask
             when S_INPUT_MASK =>
-                o_en <= '1'; -- X punto da lavutare
+                o_en <= '1'; -- X punto da valutare
             when S_COORD_X =>
                 o_en <= '1'; -- Y punto da valutare
             when S_COORD_Y|S_C1Y|S_C2Y|S_C3Y|S_C4Y|S_C5Y|S_C6Y|S_C7Y =>
